71468ef8fcc3773017c76ecc12f9f9dcfeae5dc9
Louikka <lolikadimonaa@gmail.com>
2026-08-27 14:36:23 +0300

    added more tests + corrected existed

--- interpreter/scanner/scanner_test.go
+++ interpreter/scanner/scanner_test.go
@@ -5,16 +5,41 @@ import (
 )
 
 func TestScannerInitialization(t *testing.T) {
-	const test_str = "123"
-
-	scanner := _NewScanner(test_str)
-
-	if scanner.s != test_str {
-		t.Errorf("Strings not matched => \"%s\" != \"%s\".", test_str, scanner.s)
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "0",
+			expected: "0",
+		},
+		{
+			input:    " 1 + 2",
+			expected: "1 + 2",
+		},
+		{
+			input:    "  	 3 - 4     		",
+			expected: "3 - 4",
+		},
+		{
+			input:    "   pi * 10 ",
+			expected: "PI * 10",
+		},
+		{
+			input:    " Tan( 10 / 3 ) ",
+			expected: "TAN( 10 / 3 )",
+		},
 	}
 
-	if scanner.pos != 0 {
-		t.Errorf("Wrong current position initialization => %d.", scanner.pos)
+	for _, test := range tests {
+		scanner := _NewScanner(test.input)
+
+		if scanner.s != test.expected {
+			t.Errorf("\"%s\" => strings mismatched (got \"%s\", expected \"%s\")", test.input, scanner.s, test.expected)
+		}
+		if scanner.pos != 0 {
+			t.Errorf("\"%s\" => wrong initial position (got %d instead of 0).", test.input, scanner.pos)
+		}
 	}
 }
 
@@ -25,40 +50,131 @@ func TestScannerCoreFunctions(t *testing.T) {
 	scanner := _NewScanner(test_str)
 
 	peekedChar, err := scanner.peek(0)
-	if err != nil || peekedChar != test_str[0] {
-		t.Errorf("Scanner.peek() error or peeking at wrong poition => error or mismatched char %c != %c.", test_str[0], peekedChar)
+	if err != nil {
+		t.Errorf("peek() => %s", err)
+	}
+	if peekedChar != test_str[0] {
+		t.Errorf("peek() => (peeking at pos 0) mismatched char (\"%c\" instead of \"%c\")", test_str[0], peekedChar)
 	}
 
 	nextChar, err := scanner.next()
-	if err != nil || nextChar != test_str[1] {
-		t.Errorf("Scanner.next() error => error or mismatched char \"%c\" != \"%c\".", test_str[1], nextChar)
+	if err != nil {
+		t.Errorf("next() => %s", err)
+	}
+	if nextChar != test_str[1] {
+		t.Errorf("next() => mismatched char (\"%c\" instead of \"%c\")", test_str[1], nextChar)
 	}
 
 	peekedChar, err = scanner.peek(1)
-	if err != nil || peekedChar != test_str[2] {
-		t.Errorf("Scanner.peek() error or peeking at wrong poition => error or mismatched char %c != %c.", test_str[2], peekedChar)
+	if err != nil {
+		t.Errorf("peek() => %s", err)
+	}
+	if peekedChar != test_str[2] {
+		t.Errorf("peek() => (peeking at pos 2) mismatched char (\"%c\" instead of \"%c\")", test_str[2], peekedChar)
 	}
 
-	scanner.pos = test_strlen
+	// set position to the last
+	scanner.pos = test_strlen - 1
 
-	if !scanner.isEndOfString() {
-		t.Errorf("Cannot determine if end of string => current position %d in string with length of %d.", scanner.pos, test_strlen)
+	if !scanner.isLast() {
+		t.Errorf("isLast() => current position %d in string with length of %d", scanner.pos, test_strlen)
 	}
 
-	scanner2 := _NewScanner("abcdefghjk")
-	for i := 0; !scanner2.isEndOfString(); i++ {
-		_, err := scanner2.peek(0)
+	scanner.pos++
+
+	if !scanner.isEnd() {
+		t.Errorf("isEnd() => current position %d in string with length of %d", scanner.pos, test_strlen)
+	}
+
+	scanner = _NewScanner("abcdefg123456789")
+	for i := 0; !scanner.isEnd(); i++ {
+		_, err := scanner.peek(0)
 		if err != nil {
-			t.Errorf("Iteration failed => Scanner.peek() error at iteration %d.", i)
+			t.Errorf("iteration failed => peek() error at iteration %d : %s", i, err)
 		}
 
-		scanner2.next()
+		if !scanner.isLast() {
+			_, err = scanner.next()
+			if err != nil {
+				t.Errorf("iteration failed => next() error at iteration %d : %s", i, err)
+			}
+		} else {
+			break
+		}
+	}
+}
+
+func TestScannerReadKeyword(t *testing.T) {
+	tests := []struct {
+		s      string
+		ttype  TokenType
+		tvalue string
+	}{
+		{
+			s:      "PI",
+			ttype:  TOKEN_TYPE_CONSTANT,
+			tvalue: "PI",
+		},
+		{
+			s:      "E",
+			ttype:  TOKEN_TYPE_CONSTANT,
+			tvalue: "E",
+		},
+		{
+			s:      "NONEXISTINGCONSTANT",
+			ttype:  TOKEN_TYPE_CONSTANT,
+			tvalue: "NONEXISTINGCONSTANT",
+		},
+		{
+			s:      "COS(12.3)",
+			ttype:  TOKEN_TYPE_FUNCTION,
+			tvalue: "COS",
+		},
+		{
+			s:      "ABS()",
+			ttype:  TOKEN_TYPE_FUNCTION,
+			tvalue: "ABS",
+		},
+		{
+			s:      "NONEXISTINGFUNCTION()",
+			ttype:  TOKEN_TYPE_FUNCTION,
+			tvalue: "NONEXISTINGFUNCTION",
+		},
+	}
+
+	for _, test := range tests {
+		scanner := _NewScanner(test.s)
+
+		token, err := scanner.readKeyword()
+		if err != nil {
+			t.Errorf("\"%s\" => %s", test.s, err)
+		}
+
+		if token.Type != test.ttype {
+			t.Errorf("\"%s\" => token type mismatched (got \"%s\", expected \"%s\")", test.s, token.Type, test.ttype)
+		}
+		if token.Value != test.tvalue {
+			t.Errorf("\"%s\" => token value mismatched (got \"%s\", expected \"%s\")", test.s, token.Value, test.tvalue)
+		}
 	}
 }
 
 func TestScannerOutput(t *testing.T) {
-	_, err := Scan("1 + 2")
-	if err != nil {
-		t.Errorf("Error while Scan() => %v.", err)
+	tests := []string{
+		"0",
+		"1 + 2",
+		"1 + 2 * 3",
+		"(1 + 2) * 3",
+		"4 - PI",
+		"SIN(5) / 6",
+		"7 ^ 8",
+		"ABS(-9)",
+	}
+
+	for _, test := range tests {
+		_, err := Scan(test)
+		if err != nil {
+			t.Errorf("\"%s\" => %s", test, err)
+		}
 	}
 }
